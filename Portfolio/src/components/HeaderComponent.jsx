bc09b143058b9a53477c2a85fe5e45d52c0fcc36
Guada-lupes <gma.pedagoga@gmail.com>
2025-04-05 21:08:56 +0100

    resets, homepage, header, footer

---
+++ Portfolio/src/components/HeaderComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../styles/HeaderComponentStyle.css"
+
+export const HeaderComponent = () => {
+  return (
+    <header className='header'>HeaderComponent</header>
+  )
+}
