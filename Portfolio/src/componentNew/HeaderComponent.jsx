5f26dec5a96f2c65af9a660b709dc06b4401c92d
Guadalupe <gma.pedagoga@gmail.com>
2025-09-19 12:56:58 +0100

    añadida animación en título e iconos de entrada

--- Portfolio/src/componentNew/HeaderComponent.jsx
+++ Portfolio/src/componentNew/HeaderComponent.jsx
@@ -1,26 +1,56 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect } from "react";
 import { Link } from "react-router-dom";
 import "../styleNew/HeaderComponentStyle.css";
 import { useLocation } from "react-router-dom";
 import { ToggleThemeComponent } from "./ToggleThemeComponent";
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
 
 export const HeaderComponent = () => {
   const location = useLocation();
   const url = location.pathname;
 
-useEffect(()=>{
+useLayoutEffect(() => {
+  document.fonts.ready.then(() => {
+    gsap.registerPlugin(SplitText);
+    
+    const split = new SplitText("#compatibility", { type: "words, chars" });
+    const tl = gsap.timeline();
 
-}, [url])
+    tl.set("#compatibility", { autoAlpha: 1 });
 
+    split.chars.forEach((char, i) => {
+      tl.fromTo(
+        char,
+        {
+          transformOrigin: "center -160px",
+          z: 0.1,
+          rotation: Math.random() < 0.5 ? 90 : -90,
+        },
+        {
+          rotation: 0,
+          ease: "elastic.out",
+          duration: 2.4,
+        },
+        0.3 + i * 0.06
+      );
+    });
+  });
+}, []);
+
+  useEffect(() => {
+    
+  }, [url]);
 
   return (
     <header className="header">
-<ToggleThemeComponent/>
+      <ToggleThemeComponent />
       <div className="header-info">
-        <h1>
-          <Link to="/">Guadalupe Marándola</Link>
-        </h1>
+        <Link to="/">
+          <h1 id="compatibility" className="name_title"> Guadalupe Marándola</h1>
+        </Link>
+
         <p>Desarrollo Web + Educación</p>
       </div>
       {/* navegador para escritorio */}
@@ -28,13 +58,25 @@ useEffect(()=>{
         <nav className="header-nav">
           <ul className="header-nav-ul">
             <li>
-              <Link to="/" className={url === "/" ? "titles-checked" :""}>Inicio</Link>
+              <Link to="/" className={url === "/" ? "titles-checked" : ""}>
+                Inicio
+              </Link>
             </li>
             <li>
-              <Link to="/aboutme" className={url === "/aboutme" ? "titles-checked" : ""}>Sobre mi</Link>
+              <Link
+                to="/aboutme"
+                className={url === "/aboutme" ? "titles-checked" : ""}
+              >
+                Sobre mi
+              </Link>
             </li>
             <li>
-              <Link to="/contact" className={url === "/contact" ? "titles-checked" : ""}>Contacto</Link>
+              <Link
+                to="/contact"
+                className={url === "/contact" ? "titles-checked" : ""}
+              >
+                Contacto
+              </Link>
             </li>
           </ul>
         </nav>
--- Portfolio/src/componentNew/IconosComponent.jsx
+++ Portfolio/src/componentNew/IconosComponent.jsx
@@ -1,38 +1,84 @@
-import React from 'react'
-import foto1 from "../assets/obrera.png"
-import foto2 from "../assets/ordenador.png"
-import foto3 from "../assets/caja.png"
-import foto4 from "../assets/libro.png"
-import foto5 from "../assets/herramientas.png"
-import { Link } from 'react-router-dom'
-import "../styleNew/IconosComponentStyle.css"
+import React, { useEffect } from "react";
+import foto1 from "../assets/obrera.png";
+import foto2 from "../assets/ordenador.png";
+import foto3 from "../assets/caja.png";
+import foto4 from "../assets/libro.png";
+import foto5 from "../assets/herramientas.png";
+import { Link } from "react-router-dom";
+import "../styleNew/IconosComponentStyle.css";
+import { gsap } from "gsap";
 
 export const IconosComponent = () => {
+  useEffect(() => {
+    gsap.to(".imagen", {
+  scale: 1,
+  opacity: 1,
+  duration: 0.2,
+  stagger: 0.1,
+  ease: "circ.out",
+    });
+  }, []);
+
   return (
-    <section className='home-iconos-section'>
-        <div className='iconos-container'>
-            <div className='icono-item'>
-                <Link className='item-img' to="/proyects"><img src={foto2} alt="icono de un ordenador retro" /></Link>
-                <Link className='item-p' to="/proyects"><p>Proyectos Web-Dev</p></Link> 
-                
-            </div>
-            <div className='icono-item'>
-                <Link className='item-img' to="/studies"><img src={foto4} alt="icono de un libro"/></Link>
-                <Link to="/studies" className='item-p'><p>Formación</p></Link> 
-            </div>
-            <div className='icono-item'>
-                <Link className='item-img' to="/tech"><img src={foto5} alt="icono de una caja de herramientas" /></Link>
-                <Link to="/work" className='item-p'><p>Tecnologías</p></Link>
-            </div>
-            <div className='icono-item'>
-                <Link className='item-img' to="/work"><img src={foto1} alt="icono de una mujer obrera" /></Link>
-                <Link to="/work" className='item-p'><p>Experiencia Profesional</p></Link>
-            </div>
-            <div className='icono-item'>
-                <Link className='item-img' to="/others"><img src={foto3} alt="icono de una caja de sorpresa" /></Link>
-                <Link to="/others" className='item-p'><p>Otros proyectos</p></Link>
-            </div>
+    <section className="home-iconos-section">
+      <div className="iconos-container">
+        <div className="icono-item">
+          <Link className="item-img" to="/proyects">
+            <img
+              className="imagen"
+              src={foto2}
+              alt="icono de un ordenador retro"
+            />
+          </Link>
+          <Link className="item-p" to="/proyects">
+            <p>Proyectos Web-Dev</p>
+          </Link>
         </div>
+        <div className="icono-item">
+          <Link className="item-img" to="/studies">
+            <img className="imagen" src={foto4} alt="icono de un libro" />
+          </Link>
+          <Link to="/studies" className="item-p">
+            <p>Formación</p>
+          </Link>
+        </div>
+        <div className="icono-item">
+          <Link className="item-img" to="/tech">
+            <img
+              className="imagen"
+              src={foto5}
+              alt="icono de una caja de herramientas"
+            />
+          </Link>
+          <Link to="/work" className="item-p">
+            <p>Tecnologías</p>
+          </Link>
+        </div>
+        <div className="icono-item">
+          <Link className="item-img" to="/work">
+            <img
+              className="imagen"
+              src={foto1}
+              alt="icono de una mujer obrera"
+            />
+          </Link>
+          <Link to="/work" className="item-p">
+            <p>Experiencia Profesional</p>
+          </Link>
+        </div>
+        <div className="icono-item">
+          <Link className="item-img" to="/others">
+            <img
+              className="imagen"
+              src={foto3}
+              alt="icono de una caja de sorpresa"
+            />
+          </Link>
+          <Link to="/others" className="item-p">
+            <p>Otros proyectos</p>
+          </Link>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
