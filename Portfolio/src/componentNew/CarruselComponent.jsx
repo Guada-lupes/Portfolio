adbed411cb88105c00a3f245ddce9d3423e681b8
Guadalupe <gma.pedagoga@gmail.com>
2025-09-19 18:45:14 +0100

    arreglados botones, falta que el carrusel funcione bien en responsive

--- Portfolio/src/componentNew/CarruselComponent.jsx
+++ Portfolio/src/componentNew/CarruselComponent.jsx
@@ -12,7 +12,6 @@ export const CarruselComponent = () => {
   const proyectsAmount = data.length;
   const pistaRef = useRef(null);
 
-
   function handleNext() {
     if (pistaRef.current) {
       pistaRef.current.scrollBy({
@@ -20,7 +19,6 @@ export const CarruselComponent = () => {
         behavior: "smooth",
       });
     }
-    console.log("hola")
   }
   function handleBefore() {
     if (pistaRef.current) {
@@ -33,18 +31,29 @@ export const CarruselComponent = () => {
 
   return (
     <section className="carrusel-container">
-      <button
-          className="carrusel-button-before"
+      <div className="buttons_container">
+        <div
+          className="carrusel-button before"
           onClick={(e) => handleBefore(e)}
         >
-          ◀
-        </button>
-      <div className="carrusel-pista" ref={pistaRef}>
-        {data.map((objeto, index) => (
-          <div
-            className={"carrusel-item-container"}
-            key={index}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1"
+            stroke="grey"
+            class="size-6"
           >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        </div>
+        <div className="carrusel-pista" ref={pistaRef}>
+          {data.map((objeto, index) => (
+            <div className={"carrusel-item-container"} key={index}>
               <div className="carrusel-item">
                 <div className="carrusel-img-container">
                   <img src={objeto.imagen} alt={objeto.alt} />
@@ -54,15 +63,26 @@ export const CarruselComponent = () => {
                 </div>
                 <GitHubLogoPath path={objeto.em} />
               </div>
-          </div>
-        ))}
+            </div>
+          ))}
+        </div>
+        <div className="carrusel-button after" onClick={(e) => handleNext(e)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1"
+            stroke="grey"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+        </div>
       </div>
-      <button
-          className="carrusel-button-after"
-          onClick={(e) => handleNext(e)}
-        >
-          ▶
-        </button>
     </section>
   );
 };
