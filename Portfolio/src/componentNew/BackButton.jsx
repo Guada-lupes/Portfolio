5ada3f6a52eb2a8f265310e54c9d9c920574a6a4
Guadalupe <gma.pedagoga@gmail.com>
2025-09-30 17:08:11 +0100

    mas estilos

--- Portfolio/src/componentNew/BackButton.jsx
+++ Portfolio/src/componentNew/BackButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const BackButton = () => {
-const style = {color: "var(--text1)", fontSize: "1.2rem", textAlign: "center", fontFamily: "Proxima Nova", border: "solid 0.5px", borderRadius: "10px", justifySelf: "center", padding: "1.2rem", width: "fit-content", marginTop: "15vh"}
+const style = {color: "var(--text1)", textAlign: "center", fontFamily: "Proxima Nova", border: "solid 0.5px", borderRadius: "10px", justifySelf: "center", padding: "1.2rem", width: "fit-content", marginTop: "15vh"}
 
     return <Link to="/"><p style={style}>Volver</p></Link>;
 };
